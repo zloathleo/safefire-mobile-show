79062c93d0099f38ad9aa1c11de1cf3c85d81804
zloathleo <zloathleo@gmail.com>
2017-04-25 15:31:21 +0800

    格式化

--- src/scripts/components/MyRoutes.js
+++ src/scripts/components/MyRoutes.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-
+ 
 //custom
 import Login from "./Login";
-import Main from "./Main";
-
+import Main from "./Main"; 
 import Overview from "./Overview";
 import Analyze from "./Analyze";
 
